dc640d2bb1aa894508774dcd3bb38b8bbd0d7adb
NateDiamond13 <nate.diamond13@gmail.com>
2024-09-16 19:55:00 -0700

    Switch back to rusty_ytdl (maybe fix?)

--- discord_bot/src/commands/utils/videos.rs
+++ discord_bot/src/commands/utils/videos.rs
@@ -13,29 +13,10 @@ pub struct VideoDetails {
 
 pub async fn get_video_details(url_or_search: &str, data: &CommandData) -> Result<VideoDetails> {
     let url_or_search = url_or_search.trim().to_string();
-
-    let url = if is_valid_url(&url_or_search) {
-        url_or_search.clone()
-    } else {
-        get_url_by_search(&url_or_search).await?
-    };
-
-    let mut src = YoutubeDl::new(data.http_client.clone(), url);
-    let metadata = src.aux_metadata().await?;
-
-    Ok(VideoDetails {
-        input: src.into(),
-        num_seconds: metadata.duration.ok_or(Error::VideoDetailParse)?.as_secs(),
-        source_url: metadata.source_url.ok_or(Error::VideoDetailParse)?,
-    })
-}
-
-pub async fn _get_video_details(url_or_search: &str, data: &CommandData) -> Result<VideoDetails> {
-    let url_or_search = url_or_search.trim().to_string();
     if is_valid_url(&url_or_search) {
-        if !_is_valid_youtube_url(&url_or_search) {
+        if is_valid_youtube_url(&url_or_search) {
             // If it's a valid YouTube url, find it directly with rusty_ytdl
-            _get_youtube_video_by_url(&url_or_search, data).await
+            get_youtube_video_by_url(&url_or_search, data).await
         } else {
             // If it's a valid non-YouTube url, find it with YoutubeDl
             let mut src = YoutubeDl::new(data.http_client.clone(), url_or_search);
@@ -50,20 +31,11 @@ pub async fn _get_video_details(url_or_search: &str, data: &CommandData) -> Resu
     } else {
         // Otherwise search for it with rusty_ytdl
         let url = get_url_by_search(&url_or_search).await?;
-        //get_youtube_video_by_url(&url, data).await
-
-        let mut src = YoutubeDl::new(data.http_client.clone(), url);
-        let metadata = src.aux_metadata().await?;
-
-        Ok(VideoDetails {
-            input: src.into(),
-            num_seconds: metadata.duration.ok_or(Error::VideoDetailParse)?.as_secs(),
-            source_url: metadata.source_url.ok_or(Error::VideoDetailParse)?,
-        })
+        get_youtube_video_by_url(&url, data).await
     }
 }
 
-async fn _get_youtube_video_by_url(url: &str, data: &CommandData) -> Result<VideoDetails> {
+async fn get_youtube_video_by_url(url: &str, data: &CommandData) -> Result<VideoDetails> {
     let video_options = VideoOptions {
         quality: VideoQuality::Lowest,
         filter: VideoSearchOptions::Audio,
@@ -90,7 +62,7 @@ async fn get_url_by_search(search: &str) -> Result<String> {
     }
 }
 
-fn _is_valid_youtube_url(url: &str) -> bool {
+fn is_valid_youtube_url(url: &str) -> bool {
     rusty_ytdl::get_video_id(url).is_some()
 }
 
