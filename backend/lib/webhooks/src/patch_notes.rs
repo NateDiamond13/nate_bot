245602aabeeb9ea47db4a83c59dfe204fa781a53
NateDiamond13 <nate.diamond13@gmail.com>
2024-11-22 13:42:27 -0800

    Fixed html->markdown parsing for patch notes

--- backend/lib/webhooks/src/patch_notes.rs
+++ backend/lib/webhooks/src/patch_notes.rs
@@ -33,20 +33,35 @@ pub fn create_patch_embed(patch_notes: &PatchNotes) -> CreateEmbed<'_> {
 
 fn format_embed_description(content: &str) -> String {
     let max_lines = 8;
-    let max_length = 4096;
+    let pref_max_chars = 500;
+    let trunc_length = 4096;
 
-    let parsed = content.replace("<br>", "");
-    let lines = parsed
+    let Ok(markdown_str) = htmd::convert(content) else {
+        return "".to_string();
+    };
+
+    let lines = markdown_str
         .lines()
+        .map(|s| s.trim())
         .filter(|s| !s.is_empty())
         .collect::<Vec<&str>>();
 
-    let mut description = if lines.len() > max_lines {
-        [&lines[0..max_lines - 1], &["..."]].concat()
+    let mut total_lines = 0;
+    let mut char_count = 0;
+    for line in lines.iter().take(max_lines) {
+        total_lines += 1;
+        char_count += line.len();
+        if char_count >= pref_max_chars {
+            break;
+        }
+    }
+
+    let mut description = if lines.len() > total_lines {
+        [&lines[0..total_lines], &["..."]].concat()
     } else {
         lines
     }
     .join("\n");
-    description.truncate(max_length);
+    description.truncate(trunc_length);
     description
 }
